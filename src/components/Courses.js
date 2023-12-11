3b5dab7d0353d29c1c31f2cdbe0d1ead2e79c04d
Tonio Adams <avcx12@gmail.com>
2023-12-10 19:32:05 -0700

    Update create course field

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -34,7 +34,7 @@ const Courses = () => {
 
         <Link
           className="course--module course--add--module"
-          to="create-course.html"
+          to="/courses/create"
         >
           <span className="course--add--title">
             <svg
