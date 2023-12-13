a4e097d091f210f3a94389438050cb98510763b8
Tonio Adams <avcx12@gmail.com>
2023-12-13 10:25:57 -0700

    Added Comments

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,10 +3,24 @@ import Cookies from "js-cookie";
 import { api } from "../utils/apihelper";
 const UserContext = createContext(null);
 
+/**
+ * UserProvider supplies 'UserContext' to enclosed components.
+ *
+ * @param {Components to be provided the 'UserContext' context} props
+ * @returns A provider element with the 'authUser' and relevant actions
+ */
+
 export const UserProvider = (props) => {
   const cookie = Cookies.get("authorizedUser");
   const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
 
+  /**
+   * signIn - attempts to sign in a user using the supplied credentials
+   * returns the user and stores the credentials in the 'authorizedUser' cookie.
+   *
+   * @param {object} credentials - object containing 'username' and 'password' properties.
+   * @returns {object} user on successful sign in attempt
+   */
   const signIn = async (credentials) => {
     const response = await api("/users", "GET", null, credentials);
     if (response.status === 200) {
@@ -21,6 +35,10 @@ export const UserProvider = (props) => {
       throw new Error();
     }
   };
+
+  /**
+   * signOut - signs out of the current user and removes 'authorizedUser' cookie
+   */
 
   const signOut = () => {
     Cookies.remove("authorizedUser");
