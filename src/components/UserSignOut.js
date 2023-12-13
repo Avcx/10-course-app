658349a5e3e4cf1ecf38a17029002f825f739200
Tonio Adams <avcx12@gmail.com>
2023-12-13 10:27:41 -0700

    Added Comments

--- src/components/UserSignOut.js
+++ src/components/UserSignOut.js
@@ -1,6 +1,13 @@
 import { useContext, useEffect } from "react";
 import UserContext from "../context/UserContext";
 import { Navigate } from "react-router-dom";
+
+/**
+ * UserSignOut - signs the current user out and redirects them to the index.
+ *
+ * @returns Navigate component that returns the user to the index route.
+ */
+
 const UserSignOut = () => {
   const { actions } = useContext(UserContext);
   useEffect(() => {
